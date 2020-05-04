e44600f2516a5fbd1fd9bef06d746ccba5d502d0
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-04 15:17:50 -0500

    setting up files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01_Exception_Handling_and_Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
