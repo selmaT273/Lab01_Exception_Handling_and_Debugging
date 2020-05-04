1f78bc46d3bbede91bca2a8a90c8f3ff2ddab87a
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-04 18:07:57 -0500

    laid out the beginning logic and layout of methods

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace Lab01_Exception_Handling_and_Debugging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartSequence();
         }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Enter a number greater than zero.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int[] newArray = new int[userInput];
+
+        }
+
+        static int[] Populate(intArray)
+        {
+
+        }
+
+        static int GetSum(intArray)
+        {
+
+        }
+
+        static int GetProduct(intArray, intSum)
+        {
+
+        }
+
+        static decimal GetQuotient(intProduct)
+        {
+
+        }
+
     }
 }
