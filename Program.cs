05403212ea88b412819ed73811639a79b72cf8eb
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-05 11:06:29 -0500

    added comments for first half

--- Program.cs
+++ Program.cs
@@ -10,14 +10,17 @@ namespace Lab01_Exception_Handling_and_Debugging
 
             try
             {
+                // Calling the method that calls Populate, GetSum, GetProduct, and GetQuotient
                 StartSequence();
             }
             catch(Exception ex)
             {
+                // Generic exception to handle if it hasnt been caught by another type of exception
                 Console.Write("Whoops, something went wrong.", ex.Message);
             }
             finally
             {
+                // Always ends the game with this string
                 Console.Write("Program is complete");
             }
         }
@@ -26,41 +29,64 @@ namespace Lab01_Exception_Handling_and_Debugging
         {
             try
             {
-
+                // Prompt the user to enter a number & convert it to an int
                 Console.WriteLine("Enter a number greater than zero.");
                 int userInput = Convert.ToInt32(Console.ReadLine());
 
+                // Create a new empty array with the array length equal to the user's input
                 int[] newArray = new int[userInput];
 
-
+                // Call Populate and then convert its return value to a string seperated by commas to show the user
                 Console.WriteLine($"The numbers in the array are {string.Join(", ", Populate(newArray))}");
+
+                // Call GetSum passing in the new array that was populated and setting it to variable sum
                 int sum = GetSum(newArray);
+
+                // Printing out sum to user
                 Console.WriteLine($"The sum of the array is {sum}");
+
+                // Call GetProduct passing in the populated newArray and the sum variable
                 int product = GetProduct(newArray, sum);
+
+                // Call GetQuotient passing in the product variable
                 GetQuotient(product);
             }
             catch (FormatException fex)
             {
+                // Handle formatting errors and gracefully tell the user:
                 Console.WriteLine("Oops, there was an error with the format.");
             }
             catch(OverflowException oex)
             {
+                // Handle overflows and gracefully tell the user:
                 Console.WriteLine("Uh oh. Overflow detected");
             }
 
         }
 
+        // Populate will return an int array
         static int[] Populate(int[] intArray)
         {
+            // Set a counter starting at one to indicate to the user how many numbers they've populated so far
             int j = 1;
 
+            // Make a for loop to iterate over each index in the new empty array
             for (int i = 0; i < intArray.Length; i++)
             {
+                // Ask the user to input a number to fill the empty indexes of the new array
                 Console.WriteLine($"Please enter a number: {j} of {intArray.Length}");
+
+                //Convert the user's input to an integer
                 intArray[i] = Convert.ToInt32(Console.ReadLine());
+
+                // Increment the counter to move onto the next index in the array
                 j++;
             }
+
+            // Tell the user what the size of the array is
             Console.WriteLine($"Your array is size: {intArray.Length}");
+
+            // Return the populated array to be sent up to StartSequence
             return intArray;
             
         }
